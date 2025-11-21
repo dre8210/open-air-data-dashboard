b917ef6beb9e966b6265f9e622455e0315f5520c
Carl Andrews <carlandrews8210@gmail.com>
2025-11-21 18:05:47 +0000

    Added Python Database Manager Script to aid in the creation and destruction on databases

--- pipeline/database_manager.py
+++ pipeline/database_manager.py
@@ -6,92 +6,80 @@ import logging
 from duckdb import DuckDBPyConnection
 import duckdb as ddb
 
+
 def connect_to_database(path: str) -> DuckDBPyConnection:
-    """Connects to the database and returns a connection object."""
-
-    logging.info("Connecting to database: {path}")
-
+    logging.info(f"Connecting to database at {path}")
     con = ddb.connect(path)
     con.sql("""
-        SET s3_access_key_id=' ';
-        SET s3_secret_access_key=' ';
-        SET s3_region=' ';
-    """)
-
+        SET s3_access_key_id='';
+        SET s3_secret_access_key='';
+        SET s3_region='';
+        """)
     return con
 
 
 def close_database_connection(con: DuckDBPyConnection) -> None:
-    """Closes the database connection."""
-
-    logging.info(f"Closing database connection") 
-
+    logging.info(f"Closing database connection")
     con.close()
 
 
 def collect_query_paths(parent_dir: str) -> List[str]:
-    """Collects all query paths from the parent directory."""
-
+    
     sql_files = []
+
     for root, _, files in os.walk(parent_dir):
         for file in files:
             if file.endswith(".sql"):
                 file_path = os.path.join(root, file)
                 sql_files.append(file_path)
-
-    logging.info(f"Found {len(sql_files)} sql scripts at locations {parent_dir}")
-
+    
+    logging.info(f"Found {len(sql_files)} sql scripts at location {parent_dir}")
+    
     return sorted(sql_files)
 
 
 def read_query(path: str) -> str:
-    """Reads the query from the given path."""
-
     with open(path, "r") as f:
         query = f.read()
         f.close()
-
     return query
-    
+
 
 def execute_query(con: DuckDBPyConnection, query: str) -> None:
-    """Executes the query on the database."""
-
     con.execute(query)
-    
+
 
 def setup_database(database_path: str, ddl_query_parent_dir: str) -> None:
-    """Sets up the database by creating tables and loading data."""
-    query_paths= collect_query_paths(ddl_query_parent_dir)
+    
+    query_paths = collect_query_paths(ddl_query_parent_dir)
 
-    con= connect_to_database(database_path)
+    con = connect_to_database(database_path)
 
     for query_path in query_paths:
-        query= read_query(query_path)
+        query = read_query(query_path)
         execute_query(con, query)
         logging.info(f"Executed query from {query_path}")
-
+    
     close_database_connection(con)
 
 
 def destroy_database(database_path: str) -> None:
-    """Destroys the database by removing the file."""
-
     if os.path.exists(database_path):
         os.remove(database_path)
-            
+
 
 def main():
+
     logging.getLogger().setLevel(logging.INFO)
+    
+    parser = argparse.ArgumentParser(description="CLI tool to setup or destroy a database.")
 
-    parser= argparse.ArgumentParser(description="CLI tool to setup or destroy a database")
-
-    group= parser.add_mutually_exclusive_group(required=True)
+    group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--create", action="store_true", help="Create the database")
     group.add_argument("--destroy", action="store_true", help="Destroy the database")
 
     parser.add_argument("--database-path", type=str, help="Path to the database")
-    parser.add_argument("--ddl-query-parent-dir", type=str, help="Path to the directory containing the DDL queries")
+    parser.add_argument("--ddl-query-parent-dir", type=str, help="Path to the parent directory of the ddl queries")
 
     args = parser.parse_args()
 
@@ -99,7 +87,6 @@ def main():
         setup_database(database_path=args.database_path, ddl_query_parent_dir=args.ddl_query_parent_dir)
     elif args.destroy:
         destroy_database(database_path=args.database_path)
-    
 
 if __name__ == "__main__":
     main()
--- sql/ddl/1_raw_air_quality.sql
+++ sql/ddl/1_raw_air_quality.sql
@@ -1,15 +1,15 @@
-CREATE TABLE
-    IF NOT EXISTS raw.air_quality_data (
-        location_id BIGINT,
-        sensors_id BIGINT,
-        "location" VARCHAR,
-        "datetime" TIMESTAMP,
-        lat DOUBLE,
-        lon DOUBLE,
-        "parameter" VARCHAR,
-        units VARCHAR,
-        "value" DOUBLE,
-        "month" VARCHAR,
-        "year" BIGINT,
-        ingestion_datetime TIMESTAMP
-    )
+CREATE TABLE IF NOT EXISTS raw.air_quality (
+	location_id BIGINT,
+	sensors_id BIGINT,
+	"location" VARCHAR,
+	"datetime" TIMESTAMP,
+	lat DOUBLE,
+	lon DOUBLE,
+	"parameter" VARCHAR,
+	units VARCHAR,
+	"value" DOUBLE,
+	locationid BIGINT,
+	"month" VARCHAR,
+	"year" BIGINT,
+	ingestion_datetime TIMESTAMP
+);
